0139a52f3ee4d9f58f80c3c814c8e9f969a736d7
Dom Fung <fung_dominic@hotmail.com>
2023-02-08 18:29:56 -0500

    adding styles back to components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import './App.css'
 import { Chat } from './components/chat'
 import { Fab } from './components/fab'
 
--- src/components/fab.tsx
+++ src/components/fab.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import "../index.css"
 
 interface FabProps {
   setOpen: (b: boolean) => void
